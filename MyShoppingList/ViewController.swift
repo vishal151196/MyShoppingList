aab360a65ce2778dcad3f05ab31b2b7ca08b914b
Maria Metrina <maria.metrina@gmail.com>
2019-10-16 21:37:55 -0400

    vishal patel- latest changes in shopping list

--- MyShoppingList/ViewController.swift
+++ MyShoppingList/ViewController.swift
@@ -10,32 +10,61 @@ import UIKit
 
 class ViewController: UIViewController{
 
-    @IBOutlet weak var stepper: UIStepper!
-    @IBOutlet weak var quantity1: UITextField!
+    
+    @IBOutlet weak var item1: UITextField!
+    @IBOutlet weak var item2: UITextField!
+    @IBOutlet weak var item3: UITextField!
+    @IBOutlet weak var item4: UITextField!
     
     
-    @IBOutlet weak var quantity2: UITextField!
+    @IBOutlet weak var firstQty: UITextField!
+    @IBOutlet weak var secondQty: UITextField!
+    @IBOutlet weak var thirdQty: UITextField!
+    @IBOutlet weak var fourthQty: UITextField!
     
-    @IBAction func stepperAction1(_ sender: UIStepper) {
-
-        quantity1.text = String(Int(stepper.value))
+    @IBOutlet weak var stepper1: UIStepper!
+    @IBOutlet weak var stepper2: UIStepper!
+    @IBOutlet weak var stepper3: UIStepper!
+    @IBOutlet weak var stepper4: UIStepper!
+    
+    
+    @IBAction func stepfun1(_ sender: Any) {
+        firstQty.text = String(Int(stepper1.value))
     }
-
-    @IBAction func stepperAction2(_ sender: UIStepper) {
-         quantity2.text = String(Int(stepper.value))
+    
+    
+    @IBAction func stepfun2(_ sender: Any) {
+        secondQty.text = String(Int(stepper2.value))
     }
+    
+    @IBAction func stepfun3(_ sender: Any) {
+        thirdQty.text = String(Int(stepper3.value))
+    }
+    
+    @IBAction func stepfun4(_ sender: Any) {
+        fourthQty.text = String(Int(stepper4.value))
+    }
+    
+    
+   @IBAction func onCancel(_ sender: Any) {
+    
+        item1.text = ""
+        item2.text = ""
+        item3.text = ""
+        item4.text = ""
+    
+        firstQty.text = "0"
+        secondQty.text = "0"
+        thirdQty.text = "0"
+        fourthQty.text = "0"
+   }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //self.quantity.delegate = self
-        // Do any additional setup after loading the view.
-    }
 
-//    func textField(_ textfield: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String ) -> Bool {
-//        let allowedCharacters = CharacterSet.decimalDigits
-//        let characterSet = CharacterSet(charactersIn: string)
-//        return allowedCharacters.isSuperset(of: characterSet)
-//    }
+    }
     
 }
 
